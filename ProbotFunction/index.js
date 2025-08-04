d4d204911848ed155c4a809290b0dd7707280552
Jannis Metrikat <120120832+jmetrikat@users.noreply.github.com>
2025-08-04 09:57:32 +0200

    Remove probot azure adapter

--- ProbotFunction/index.js
+++
@@ -1,7 +0,0 @@
-const {
-  createProbot,
-  createAzureFunction,
-} = require("@probot/adapter-azure-functions");
-const app = require("../app/index");
-
-module.exports = createAzureFunction(app, { probot: createProbot() });
